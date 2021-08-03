d4ce3115d1413564db9f5ed67c1863f73d20d24d
manish295 <79335262+manish295@users.noreply.github.com>
2021-08-03 16:24:58 -0400

    Finished (html) login page

--- Prototype/website/ui.py
+++ Prototype/website/ui.py
@@ -1,3 +1,4 @@
+from os import name
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 
 
@@ -10,17 +11,23 @@ app.secret_key = "thisisasecret"
 def home():
     if "name" not in session:
         return redirect(url_for("login"))
-    return None
-
+    name = session["name"]
+    return render_template("index.html", name=name)
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
         name = request.form["nm"]
         session["name"] = name
-        flash("Login Sucessful!")
+        flash(f"Logged in as {name}")
+        return redirect(url_for("home"))
     
     return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session.pop("name", None)
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
